dcf82211d962169bf06b8a2661667d45033fd0e8
DevPyPedro <dev.santiago.felicio@gmail.com>
2025-03-15 11:40:36 -0300

    update

---
+++ dags/brew_dag/task/extract_data.py
@@ -0,0 +1,6 @@
+from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook #type: ignore
+import pandas as pd #type: ignore
+import requests #type: ignore
+
+def extract_data_api()->None:
+    pass
